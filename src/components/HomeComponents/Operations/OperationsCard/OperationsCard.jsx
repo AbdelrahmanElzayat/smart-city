80a397293df5efb3137d8c8f0f657e275a68b1cb
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2025-01-14 05:52:23 +0200

    card overlay delete

--- src/components/HomeComponents/Operations/OperationsCard/OperationsCard.jsx
+++ src/components/HomeComponents/Operations/OperationsCard/OperationsCard.jsx
@@ -4,7 +4,7 @@ const OperationsCard = ({ item }) => {
   return (
     <div className="OperationsCard flex flex-col items-center justify-center gap-3 p-2 border border-[#CECECE] rounded-2xl">
       <div className="imgCard rounded-2xl overflow-hidden relative w-full">
-        <div className="absolute top-0 left-0 w-full h-full bg-[#D3D3C8] opacity-50"></div>
+        {/* <div className="absolute top-0 left-0 w-full h-full bg-[#D3D3C8] opacity-50"></div> */}
         <img src={item?.image} alt="cardImg" className="w-full" />
       </div>
       <div className="CardContent text-center flex flex-col justify-center items-center gap-2">
